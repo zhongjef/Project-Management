19dd100fb1b1841de69cde59b0c6432b8536a140
Junxuan Wu <atrocitytheme@gmail.com>
2019-10-24 02:18:59 -0400

    hello world set up

--- react-phase1/src/App.js
+++ react-phase1/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 import Login from './login/Login';
 // import Signup from './Signup';
-import User from './user/User';
 
 class App extends React.Component {
   render () {
