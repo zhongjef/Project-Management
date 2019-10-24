dd4be94d5a3a7bb65eee68361177704e225c0d45
Junxuan Wu <atrocitytheme@gmail.com>
2019-10-24 13:20:41 -0400

    particles added

--- react-phase1/src/Login.js
+++ react-phase1/src/Login.js
@@ -5,13 +5,15 @@ import React from 'react';
 import "./css/login.css";
 import "./css/home.css";
 import Logo from "./assets/img/logo.png"
+import Particles from 'reactparticles.js';
 class Login extends React.Component {
   
   // From Queue.js
   // Generic handler for whenever we type in an input box.
   render() {
     return (
-    <body>
+    <div className="myLogin">
+      <Particles id="tile1"/>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
       <title> csc309 Phase 1</title>
     <header class="header-sticky header-light">
@@ -74,7 +76,7 @@ class Login extends React.Component {
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
+    </div>
     );
   }
 }
