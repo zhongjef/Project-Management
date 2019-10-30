83dba4c56e06ec2f27833682829f2620c33c1f9a
Junxuan Wu <atrocitytheme@gmail.com>
2019-10-29 22:49:44 -0400

    prepare task car

---
+++ react-phase1/src/components/Projects/Taskcard.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./css/taskcard.css"
+class Taskcard extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Taskcard;
