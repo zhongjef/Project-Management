f66a3312244d8aaa3102af9ea6068d64de139ca0
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2019-10-29 21:35:10 -0400

    Isolated the css for navbar

--- react-phase1/src/Login.js
+++ react-phase1/src/Login.js
@@ -3,94 +3,84 @@
 import React from 'react';
 // Importing components
 import './assets/css/login.css';
-import './assets/css/home.css';
 import Logo from './assets/img/logo.png';
 import Particles from 'reactparticles.js';
 
-import LoginNavbar from "./components/Navbar/LoginNavbar";
+import LoginNavbar from './components/Navbar/LoginNavbar';
 class Login extends React.Component {
-  // From Queue.js
-  // Generic handler for whenever we type in an input box.
-  render() {
-    return (
-      <div className="myLogin">
-        <Particles id="tile1" />
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-          crossorigin="anonymous"
-        />
-        <title> csc309 Phase 1</title>
-        <header class="header-sticky header-light">
-          {/* using login Navbar component */}
-          <LoginNavbar />
+	// From Queue.js
+	// Generic handler for whenever we type in an input box.
+	render() {
+		return (
+			<div className='myLogin'>
+				<Particles id='tile1' />
+				<link
+					rel='stylesheet'
+					href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+					integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
+					crossorigin='anonymous'
+				/>
+				<title> csc309 Phase 1</title>
+				<header class='header-sticky header-light'>
+					{/* using login Navbar component */}
+					<LoginNavbar />
+				</header>
 
-        </header>
-
-        <div id="login-holder" class="container">
-          <div class="card">
-            <article class="card-body">
-              <a
-                href="src/pages/signup/signup.html"
-                class="float-right btn btn-outline-primary">
-                Sign up
-              </a>
-              <h4 class="card-title mb-4 mt-1">Sign in</h4>
-              <form>
-                <div class="form-group">
-                  <label>Your email</label>
-                  <input
-                    name=""
-                    class="form-control"
-                    placeholder="Email"
-                    type="email"
-                  />
-                </div>
-                <div class="form-group">
-                  <a class="float-right" href="/">
-                    Forgot?
-                  </a>
-                  <label>Your password</label>
-                  <input
-                    class="form-control"
-                    placeholder="******"
-                    type="password"
-                  />
-                </div>
-                <div class="form-group">
-                  <div class="checkbox">
-                    <label>
-                      {' '}
-                      <input type="checkbox" /> Save password{' '}
-                    </label>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <button type="submit" class="btn btn-primary btn-block">
-                    {' '}
-                    Login{' '}
-                  </button>
-                </div>
-              </form>
-            </article>
-          </div>
-        </div>
-        <script
-          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-          crossorigin="anonymous"></script>
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-          crossorigin="anonymous"></script>
-        <script
-          src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-          crossorigin="anonymous"></script>
-      </div>
-    );
-  }
+				<div id='login-holder' class='container'>
+					<div class='card'>
+						<article class='card-body'>
+							<a href='src/pages/signup/signup.html' class='float-right btn btn-outline-primary'>
+								Sign up
+							</a>
+							<h4 class='card-title mb-4 mt-1'>Sign in</h4>
+							<form>
+								<div class='form-group'>
+									<label>Your email</label>
+									<input name='' class='form-control' placeholder='Email' type='email' />
+								</div>
+								<div class='form-group'>
+									<a class='float-right' href='/'>
+										Forgot?
+									</a>
+									<label>Your password</label>
+									<input class='form-control' placeholder='******' type='password' />
+								</div>
+								<div class='form-group'>
+									<div class='checkbox'>
+										<label>
+											{' '}
+											<input type='checkbox' /> Save password{' '}
+										</label>
+									</div>
+								</div>
+								<div class='form-group'>
+									<button type='submit' class='btn btn-primary btn-block'>
+										{' '}
+										Login{' '}
+									</button>
+								</div>
+							</form>
+						</article>
+					</div>
+				</div>
+				<script
+					src='https://code.jquery.com/jquery-3.2.1.slim.min.js'
+					integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN'
+					crossorigin='anonymous'
+				/>
+				<script
+					src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'
+					integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q'
+					crossorigin='anonymous'
+				/>
+				<script
+					src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+					integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl'
+					crossorigin='anonymous'
+				/>
+			</div>
+		);
+	}
 }
 
 export default Login;
--- react-phase1/src/components/Navbar/LoginNavbar.js
+++ react-phase1/src/components/Navbar/LoginNavbar.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import {Navbar, Nav, NavDropdown, Form, FormControl,Button} from 'react-bootstrap';
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import './LoginNavbar.css';
 
 export default function LoginNavbar() {
-    return (
-    <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">我怎么那么帅</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-      <NavDropdown title="Login" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/signup">Sign up</NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link className="nav-item" href="/home">Home</Nav.Link>
-      <Nav.Link className="nav-item" href="#home">Projects</Nav.Link>
-      <Nav.Link className="nav-item" href="#link">Notifications</Nav.Link>
-        
-      </Nav>
-
-    </Navbar.Collapse>
-  </Navbar>);
+	return (
+		<Navbar bg='light' expand='lg'>
+			<Navbar.Brand href='#home'>我怎么那么帅</Navbar.Brand>
+			<Navbar.Toggle aria-controls='basic-navbar-nav' />
+			<Navbar.Collapse id='basic-navbar-nav'>
+				<Nav className='mr-auto'>
+					<NavDropdown title='Login' id='basic-nav-dropdown'>
+						<NavDropdown.Item href='/signup'>Sign up</NavDropdown.Item>
+					</NavDropdown>
+					<Nav.Link className='nav-item' href='/home'>
+						Home
+					</Nav.Link>
+					<Nav.Link className='nav-item' href='#home'>
+						Projects
+					</Nav.Link>
+					<Nav.Link className='nav-item' href='#link'>
+						Notifications
+					</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
