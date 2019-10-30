6a0bf38bd43910e83cea7884f1fbfd8063fff32e
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2019-10-29 22:24:52 -0400

    Divided the home page into left and right part

--- react-phase1/src/Home.jsx
+++ react-phase1/src/Home.jsx
@@ -11,29 +11,28 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<TopNav />
-				<div>
-					{/* Left part of page */}
-					<div>
-						<LeftNav />
-					</div>
-					{/* Right part of page */}
-					<div>
-						<h1>Dividing</h1>
-						<Card style={{ width: '18rem' }}>
-							<Card.Img variant='top' src='https://imgur.com/gallery/xn4cYl4.png' />
-							<Card.Body>
-								<Card.Title>Card Title</Card.Title>
-								<Card.Text>
-									Some quick example text to build on the card title and make up the bulk of the
-									card's content.
-								</Card.Text>
-							</Card.Body>
-							<Card.Body>
-								<Card.Link href='#'>Card Link</Card.Link>
-								<Card.Link href='#'>Another Link</Card.Link>
-							</Card.Body>
-						</Card>
+				<div class='container-fluid p-0'>
+					<TopNav />
+					<div class='row'>
+						<div class='col'>
+							<LeftNav />
+						</div>
+						<div class='col'>
+							<Card style={{ width: '18rem' }}>
+								<Card.Img variant='top' src='https://imgur.com/gallery/xn4cYl4.png' />
+								<Card.Body>
+									<Card.Title>Card Title</Card.Title>
+									<Card.Text>
+										Some quick example text to build on the card title and make up the bulk of the
+										card's content.
+									</Card.Text>
+								</Card.Body>
+								<Card.Body>
+									<Card.Link href='#'>Card Link</Card.Link>
+									<Card.Link href='#'>Another Link</Card.Link>
+								</Card.Body>
+							</Card>
+						</div>
 					</div>
 				</div>
 			</div>
---
+++ react-phase1/src/components/Panel-Tasks.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+class TaskOverview extends Component {
+	state = {};
+	render() {
+		return (
+			<ul>
+				<li />
+			</ul>
+		);
+	}
+}
+
+export default TaskOverview;
