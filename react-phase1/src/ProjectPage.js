4ccf3c7671eeb3b6b47a9ae3ec1facdf60702a42
Junxuan Wu <atrocitytheme@gmail.com>
2019-10-29 22:44:25 -0400

    add project components

--- react-phase1/src/ProjectPage.js
+++ react-phase1/src/ProjectPage.js
@@ -1,9 +1,15 @@
 import React from "react"
 import "./assets/css/projectPage.css"
+import {ButtonToolbar} from "react-bootstrap"
+import Navbar from "./components/Projects/TopNav";
 class ProjectPage extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className="mission-panel">
+                <Navbar />
+                <ButtonToolbar>
+                </ButtonToolbar>
+            </div>
         );
     }
 }
