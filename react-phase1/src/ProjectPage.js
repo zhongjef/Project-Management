363560e298ae40ba437a50d56a97fc72ba8f8f1f
Junxuan Wu <atrocitytheme@gmail.com>
2019-10-29 23:17:25 -0400

    prepare project card

--- react-phase1/src/ProjectPage.js
+++ react-phase1/src/ProjectPage.js
@@ -1,12 +1,19 @@
 import React from "react"
 import "./assets/css/projectPage.css"
 import {ButtonToolbar} from "react-bootstrap"
+import Fade from "react-reveal/Fade"
 import Navbar from "./components/Projects/TopNav";
+import Taskbar from "./components/Projects/Taskcard";
+import Taskcard from "./components/Projects/Taskcard";
 class ProjectPage extends React.Component {
     render() {
         return (
             <div className="mission-panel">
                 <Navbar />
+                <Fade left>
+                    <h1>stretching projects...</h1>
+                </Fade>
+                <Taskcard/>
                 <ButtonToolbar>
                 </ButtonToolbar>
             </div>
--- react-phase1/src/components/Projects/Taskcard.jsx
+++ react-phase1/src/components/Projects/Taskcard.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 import "./css/taskcard.css"
+import Flip from "react-reveal/Flip"
 class Taskcard extends React.Component {
     render() {
         return (
-            <div>
-            </div>
+            <Flip>
+                <div className="taskbar">
+                    <header><h1>Task1</h1></header>
+                </div>
+                <div className="taskbar"></div>
+                <div className="taskbar"></div>
+            </Flip>
         );
     }
 }
