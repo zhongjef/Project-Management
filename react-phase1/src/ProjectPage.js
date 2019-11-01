d99f60273901acdf83bfa1839e5504a9579ff7b1
wujunxuan <835918679@qq.com>
2019-11-01 03:14:47 -0400

    prepare side bar for project page

--- react-phase1/src/ProjectPage.js
+++ react-phase1/src/ProjectPage.js
@@ -6,13 +6,51 @@ import TopNav from './components/Navbar/TopNav';
 import Taskbar from "./components/ProjectsPage/Taskcard";
 import Taskcard from "./components/ProjectsPage/Taskcard";
 import Particles from 'reactparticles.js';
+import Sidebar from "react-sidebar";
+import "./components/ProjectsPage/css/projectPage.css"
+import { relative } from "path";
 
 class ProjectPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            sidebarOpen: true
+        };
+
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+    }
+
     render() {
         return (
             <div className="mission-panel">
                 <Particles id='tile1' />
                 <TopNav />
+
+                <Sidebar
+                    sidebar={<b>Side bar</b>}
+                    open={this.state.sidebarOpen}
+                    onSetOpen={this.onSetSidebarOpen}
+                    styles={{
+                        sidebar: {
+                            zIndex: 2,
+                            position: "absolute",
+                            top: 50,
+                            bottom: 0,
+                            transition: "transform .3s ease-out",
+                            WebkitTransition: "-webkit-transform .3s ease-out",
+                            willChange: "transform",
+                            overflowY: "auto"
+                        }}}
+                >
+                <button onClick={() => this.onSetSidebarOpen(true)} className="sidebar-button">
+                    ==
+                </button>
+                </Sidebar>
                 <Taskcard/>
                 <Taskcard/>
                 
