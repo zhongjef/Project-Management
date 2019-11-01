e238a8760b1207ab5a2730c0ea53224f97f71a74
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2019-10-31 23:19:22 -0400

    Isolated the tasksboard part of Home to a class

--- react-phase1/src/Home.jsx
+++ react-phase1/src/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LoginNavbar from './components/Navbar/LoginNavbar';
 import LeftNav from './components/HomePage/LeftNav';
 import Card from 'react-bootstrap/Card';
@@ -6,8 +6,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
 import PopupAction from './components/CreateProject/PopupAction';
 import ListGroup from 'react-bootstrap/ListGroup';
+import TasksBoard from './components/HomePage/TasksBoard';
 
-class Home extends React.Component {
+class Home extends Component {
 	state = {};
 	render() {
 		return (
@@ -19,20 +20,7 @@ class Home extends React.Component {
 							<LeftNav />
 						</div>
 						<div className='col-md-9'>
-							<Card style={{ width: '18rem' }}>
-								<Card.Img variant='top' src='https://imgur.com/gallery/xn4cYl4.png' />
-								<Card.Body>
-									<Card.Title>Card Title</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make up the bulk of the
-										card's content.
-									</Card.Text>
-								</Card.Body>
-								<Card.Body>
-									<Card.Link href='#'>Card Link</Card.Link>
-									<Card.Link href='#'>Another Link</Card.Link>
-								</Card.Body>
-							</Card>
+							<TasksBoard />
 						</div>
 					</div>
 				</div>
---
+++ react-phase1/src/components/HomePage/TasksBoard.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+class TasksBoard extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				<Card style={{ width: '18rem' }}>
+					<Card.Img variant='top' src='https://imgur.com/gallery/xn4cYl4.png' />
+					<Card.Body>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of the card's
+							content.
+						</Card.Text>
+						<Card.Link href='#'>Card Link</Card.Link>
+						<Card.Link href='#'>Another Link</Card.Link>
+					</Card.Body>
+				</Card>
+				<Card style={{ width: '18rem' }}>
+					<Card.Img variant='top' src='https://imgur.com/gallery/xn4cYl4.png' />
+					<Card.Body>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of the card's
+							content.
+						</Card.Text>
+						<Card.Link href='#'>Card Link</Card.Link>
+						<Card.Link href='#'>Another Link</Card.Link>
+					</Card.Body>
+				</Card>
+			</div>
+		);
+	}
+}
+
+export default TasksBoard;
