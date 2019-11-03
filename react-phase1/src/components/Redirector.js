3435dc8aded21b0a38481cacd16043bb7810fd63
dpcjzpeter <zhiwei.zhao@mail.utoronto.ca>
2019-11-03 07:50:52 -0500

    attempted redirecting

---
+++ react-phase1/src/components/Redirector.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Redirect } from "react-router";
+import { Route } from "react-router-dom";
+
+class Redirector extends React.Component {
+    render() {
+    	return (
+
+    		);
+    }
+    
+}
