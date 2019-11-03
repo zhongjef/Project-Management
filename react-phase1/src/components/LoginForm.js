c2e06c3a1212210b64df13d32336db8cde955581
Helen Wang <helen@Helens-MacBook-Pro.local>
2019-11-02 21:15:00 -0400

    simplified bootstrap library importation

--- react-phase1/src/components/LoginForm.js
+++ react-phase1/src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import '../assets/css/login.css';
 import Logo from '../assets/img/logo.png';
@@ -83,7 +84,6 @@ class LoginForm extends React.Component {
 								</div>
 								<div class='form-group'>
 									<Link to={ targetUrl }>
-										{ 'button will be set to type submit later' }
 										<button class='btn btn-primary btn-block'> Login </button>
 									</Link>
 								</div>
@@ -91,21 +91,6 @@ class LoginForm extends React.Component {
 						</article>
 					</div>
 				</div>
-				<script
-					src='https://code.jquery.com/jquery-3.2.1.slim.min.js'
-					integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN'
-					crossorigin='anonymous'
-				/>
-				<script
-					src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'
-					integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q'
-					crossorigin='anonymous'
-				/>
-				<script
-					src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
-					integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl'
-					crossorigin='anonymous'
-				/>
 			</div>
 		);
 	}
