27134cd83374dc1e7a4b384a72cbe89405f646c2
Jeff Zhong <jefferson.zhong@mail.utoronto.ca>
2019-11-25 03:10:37 -0500

    Appearance change for profile page

--- react-phase1/src/components/Account/ProfileForm.jsx
+++ react-phase1/src/components/Account/ProfileForm.jsx
@@ -3,104 +3,104 @@ import "bootstrap/dist/css/bootstrap.css";
 import { tsImportEqualsDeclaration } from "@babel/types";
 
 class ProfileForm extends Component {
-  state = { user: this.props.getUser() };
+	state = { user: this.props.getUser() };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    // TODO: In Phase 2 call backend server to update user profile in
-    console.log("Submitted");
-    this.props.backToProfile();
-  };
+	handleSubmit = (e) => {
+		e.preventDefault();
+		// TODO: In Phase 2 call backend server to update user profile in
+		console.log("Submitted");
+		this.props.backToProfile();
+	};
 
-  handleChange = e => {
-    const user = { ...this.state.user };
-    user[e.currentTarget.name] = e.currentTarget.value;
-    this.setState({ user });
-    console.log(user);
-    this.props.updateUser(user);
-  };
+	handleChange = (e) => {
+		const user = { ...this.state.user };
+		user[e.currentTarget.name] = e.currentTarget.value;
+		this.setState({ user });
+		console.log(user);
+		this.props.updateUser(user);
+	};
 
-  render() {
-    const user = this.state.user;
+	render() {
+		const user = this.state.user;
 
-    return (
-      <form onSubmit={this.handleSubmit} className="mt-2 mr-2">
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Email</label>
-          <div className="col-sm-10">
-            <input
-              onChange={this.handleChange}
-              value={user.email}
-              type="email"
-              name="email"
-              className="form-control"
-              id="inputEmail"
-            />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Username</label>
-          <div className="col-sm-10">
-            <input
-              onChange={this.handleChange}
-              type="username"
-              name="username"
-              className="form-control"
-              id="inputUsername"
-              value={user.username}
-            />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Password</label>
-          <div className="col-sm-10">
-            <input
-              onChange={this.handleChange}
-              type="password"
-              name="password"
-              className="form-control"
-              id="inputPassword"
-              value={user.password}
-            />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Phone</label>
-          <div className="col-sm-10">
-            <input
-              onChange={this.handleChange}
-              type="phone"
-              name="phone"
-              className="form-control"
-              id="inputPhone"
-              value={user.phone}
-            />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">About</label>
-          <div className="col-sm-10">
-            <input
-              onChange={this.handleChange}
-              type="about"
-              name="about"
-              className="form-control input-lg"
-              id="inputAbout"
-              value={user.about}
-            />
-          </div>
-        </div>
+		return (
+			<form onSubmit={this.handleSubmit} className="mt-2 mr-2">
+				<div className="form-group row">
+					<label className="col-sm-2 col-form-label">Email</label>
+					<div className="col-sm-10">
+						<input
+							onChange={this.handleChange}
+							value={user.email}
+							type="email"
+							name="email"
+							className="form-control"
+							id="inputEmail"
+						/>
+					</div>
+				</div>
+				<div className="form-group row">
+					<label className="col-sm-2 col-form-label">Username</label>
+					<div className="col-sm-10">
+						<input
+							onChange={this.handleChange}
+							type="username"
+							name="username"
+							className="form-control"
+							id="inputUsername"
+							value={user.username}
+						/>
+					</div>
+				</div>
+				<div className="form-group row">
+					<label className="col-sm-2 col-form-label">Password</label>
+					<div className="col-sm-10">
+						<input
+							onChange={this.handleChange}
+							type="password"
+							name="password"
+							className="form-control"
+							id="inputPassword"
+							value={user.password}
+						/>
+					</div>
+				</div>
+				<div className="form-group row">
+					<label className="col-sm-2 col-form-label">Phone</label>
+					<div className="col-sm-10">
+						<input
+							onChange={this.handleChange}
+							type="phone"
+							name="phone"
+							className="form-control"
+							id="inputPhone"
+							value={user.phone}
+						/>
+					</div>
+				</div>
+				<div className="form-group row">
+					<label className="col-sm-2 col-form-label">About</label>
+					<div className="col-sm-10">
+						<input
+							onChange={this.handleChange}
+							type="about"
+							name="about"
+							className="form-control input-lg"
+							id="inputAbout"
+							value={user.about}
+						/>
+					</div>
+				</div>
 
-        <div className="form-group row-fluid">
-          <div className="col p-0">
-            <button type="submit" className="btn btn-primary btn-block">
-              Update
-            </button>
-          </div>
-        </div>
-      </form>
-    );
-  }
+				<div className="form-group row-fluid">
+					<div className="col p-0">
+						<button type="submit" className="btn btn-primary btn-block">
+							Update
+						</button>
+					</div>
+				</div>
+			</form>
+		);
+	}
 }
 
 export default ProfileForm;
