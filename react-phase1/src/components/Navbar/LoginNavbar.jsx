78d51833846637e6680873b6d10298cc21445061
Jeff Zhong <jefferson.zhong@mail.utoronto.ca>
2019-11-25 03:01:36 -0500

    Update hyperlink

--- react-phase1/src/components/Navbar/LoginNavbar.jsx
+++ react-phase1/src/components/Navbar/LoginNavbar.jsx
@@ -1,80 +1,73 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import profilePicture from "../../assets/img/profilepic.png";
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import profilePicture from '../../assets/img/profilepic.png';
 
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button
-} from "react-bootstrap";
-import "./LoginNavbar.css";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import './LoginNavbar.css';
 
 class LoginNavbar extends Component {
-  state = {
-    user: {
-      profilePic: profilePicture
-    }
-  };
-  render() {
-    return (
-      <Navbar expand="lg navbar-dark bg-dark">
-        <Navbar.Brand href="#home">我怎么那么帅</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link className="nav-item" href="/home">
-              Home
-            </Nav.Link>
-            <Nav.Link className="nav-item" href="/project">
-              Projects
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            {/* {this.getAccount()} */}
-            <Nav.Link className="nav-item " href="/account">
-              <img
-                className="rounded"
-                style={{ width: 25 }}
-                alt="Profile Picture"
-                src={this.state.user.profilePic}
-              ></img>
-            </Nav.Link>
-            <NavDropdown title="Login" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/signup">Sign up</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link className="nav-item" href="#link">
-              Notifications
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+	state = {
+		user: {
+			profilePic: profilePicture
+		}
+	};
+	render() {
+		return (
+			<Navbar expand='lg navbar-dark bg-dark'>
+				<Navbar.Brand href='/home'>我怎么那么帅</Navbar.Brand>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='mr-auto'>
+						<Nav.Link className='nav-item' href='/home'>
+							Home
+						</Nav.Link>
+						<Nav.Link className='nav-item' href='/project'>
+							Projects
+						</Nav.Link>
+					</Nav>
+					<Nav>
+						{/* {this.getAccount()} */}
+						<Nav.Link className='nav-item ' href='/account'>
+							<img
+								className='rounded'
+								style={{ width: 25 }}
+								alt='Profile Picture'
+								src={this.state.user.profilePic}
+							/>
+						</Nav.Link>
+						<NavDropdown title='Login' id='basic-nav-dropdown'>
+							<NavDropdown.Item href='/signup'>Sign up</NavDropdown.Item>
+						</NavDropdown>
+						<Nav.Link className='nav-item' href='#link'>
+							Notifications
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		);
+	}
 
-  getAccount = () => {
-    if (this.state.user) {
-      return (
-        <Nav.Link className="nav-item " href="/account">
-          <img
-            className="rounded"
-            style={{ width: 25 }}
-            alt="Profile Picture"
-            src={this.state.user.profilePic}
-          ></img>
-        </Nav.Link>
-      );
-    } else
-      return (
-        <NavDropdown title="Login" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/signup">Sign up</NavDropdown.Item>
-        </NavDropdown>
-      );
-  };
+	getAccount = () => {
+		if (this.state.user) {
+			return (
+				<Nav.Link className='nav-item ' href='/account'>
+					<img
+						className='rounded'
+						style={{ width: 25 }}
+						alt='Profile Picture'
+						src={this.state.user.profilePic}
+					/>
+				</Nav.Link>
+			);
+		} else
+			return (
+				<NavDropdown title='Login' id='basic-nav-dropdown'>
+					<NavDropdown.Item href='/signup'>Sign up</NavDropdown.Item>
+				</NavDropdown>
+			);
+	};
 
-  // return ()
+	// return ()
 }
 
 export default LoginNavbar;
