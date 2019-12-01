a022af6889b22a4503501fb2adf296b98c9d9ac9
Jeff Zhong <jefferson.zhong@mail.utoronto.ca>
2019-11-30 22:24:11 -0500

    Update schemas

--- backend/models/Team.js
+++ backend/models/Team.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const teamSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	managers: [String],
-	members: [String],
-	projects: [String]
+	contributors: [String],
+	pid: { type: String, required: true }
 });
 
 module.exports = mongoose.model("Team", teamSchema);
