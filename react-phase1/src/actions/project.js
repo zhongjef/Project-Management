80145653726629763ce2719f5bfb61002572ebb1
kch3coo <xia0weimo1999@gmail.com>
2019-12-01 19:15:37 -0500

    changing action code style

--- react-phase1/src/actions/project.js
+++ react-phase1/src/actions/project.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
+const DEVELOPMENT = true;
+const COMMON_END_POINT = DEVELOPMENT ? "http://localhost:8080" : "http://66.212.174.180:5000";
 //(PUT)
-const createProject = async (data, url = "http://localhost:8080/project") => {
+const createProject = async (data, url = `${COMMON_END_POINT}/project`) => {
     let r = await axios({
         method: 'put',
         url: url,
@@ -12,7 +15,7 @@ const createProject = async (data, url = "http://localhost:8080/project") => {
     return r;
 };
 //(PUT)
-const createTeam = async (data, url = "http://localhost:8080/team") => {
+const createTeam = async (data, url = `${COMMON_END_POINT}/team`) => {
     let r = await axios({
         method: 'put',
         url: url,
@@ -25,7 +28,7 @@ const createTeam = async (data, url = "http://localhost:8080/team") => {
 };
 
 //(PUT)
-const createTask = async (data, url = "http://localhost:8080/task") => {
+const createTask = async (data, url = `${COMMON_END_POINT}/task`) => {
     let r = await axios({
         method: 'put',
         url: url,
@@ -38,8 +41,8 @@ const createTask = async (data, url = "http://localhost:8080/task") => {
 };
 
 //get all your Project Status (GET)
-const getProjectInfo = async (project_id, baseUrl = "http://localhost:8080") => {
-    let url = baseUrl + "/project/" + project_id;
+const getProjectInfo = async (project_id) => {
+    let url =  `${COMMON_END_POINT}/project/${project_id}`;
     let r = await axios({
         method: 'get',
         url: url,
@@ -50,8 +53,8 @@ const getProjectInfo = async (project_id, baseUrl = "http://localhost:8080") => 
     return r;
 };
 //get all your task for a specific team (GET)
-const getTeam = async (team_id, baseUrl = "http://localhost:8080") => {
-    let url = baseUrl + "/team/" + team_id;
+const getTeam = async (team_id) => {
+    let url =`${COMMON_END_POINT}/team/${team_id}`;
     let r = await axios({
         method: 'get',
         url: url,
@@ -63,8 +66,8 @@ const getTeam = async (team_id, baseUrl = "http://localhost:8080") => {
 };
 
 //add member to a team, and Modify this member's databse info such that his contributorProject List contains this project (POST)
-const addMember = async (team_id, member_id, data, baseUrl = "http://localhost:8080") => {
-    let url = baseUrl + "/team/" + team_id + "/" + member_id;
+const addMember = async (team_id, member_id, data) => {
+    let url = `${COMMON_END_POINT}/team/${team_id}/${member_id}`;
     let r = await axios({
         method: 'post',
         data: data,
@@ -77,8 +80,8 @@ const addMember = async (team_id, member_id, data, baseUrl = "http://localhost:8
 };
 
 //update this task's worker Info, and update Team's contributors -> worker -> taskList, append (POST)
-const addTaskContributor = async (task_id, data, baseUrl = "http://localhost:8080") => {
-    let url = baseUrl + "/task/" + task_id;
+const addTaskContributor = async (task_id, data) => {
+    let url = `${COMMON_END_POINT}/task/${task_id}`;
     let r = await axios({
         method: 'patch',
         data: data,
@@ -90,8 +93,8 @@ const addTaskContributor = async (task_id, data, baseUrl = "http://localhost:808
     return r;
 };
 //update this task's worker Info, and update Team's contributors -> worker -> taskList, deletee (DELETEE)
-const deleteTaskContributor = async (task_id, data, baseUrl = "http://localhost:8080") => {
-    let url = baseUrl + "/task/" + task_id;
+const deleteTaskContributor = async (task_id, data) => {
+    let url = `${COMMON_END_POINT}/task/${task_id}`;
     let r = await axios({
         method: 'delete',
         data: data,
@@ -103,8 +106,8 @@ const deleteTaskContributor = async (task_id, data, baseUrl = "http://localhost:
     return r;
 };
 //update this task's progress (PATCH)
-const updateTaskProgress = async (data, baseUrl = "http://localhost:8080") => {
-    let url = baseUrl + "/task/updateProgress";
+const updateTaskProgress = async (data) => {
+    let url = `${COMMON_END_POINT}/task/updateProgress`;
     let r = await axios({
         method: 'patch',
         data: data,
