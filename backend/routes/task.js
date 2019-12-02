43748ee35f3d9d1d5164e6bf0a790c006edcce5b
wujunxuan <835918679@qq.com>
2019-12-01 23:36:57 -0500

    add task

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -26,24 +26,56 @@ router.get("/:id", (req, res) => {
 		});
 });
 
-router.put("/:task_id", (req, res) => {
-	let taskId = req.params.task_id;
-	let name = req.body.name || "";
+router.put("/", (req, res) => {
+	console.log("creating task!");
+	let name = req.body.name || "1";
 	let desc = req.body.description || "";
 	let contributors = req.body.contributors || []
 	let isFinished = req.body.isFinished || false;
 	let progress = req.body.progress || 0;
+	console.log();
 	Task.create({ name: name, 
 		description: desc, 
 		contributors: contributors,
 		isFinished: isFinished,
 		progress: progress})
 		.then((proj) => {
+			
 			res.status(200).send("successful!");
 		})
 		.catch((err) => {
+			console.log(err);
 			res.status(500).send("failed when trying to save the target!");
 		});
+
+});
+
+router.patch("/updateProgress", (req, res) => {
+	let taskId = req.body.taskId;
+	let progress = req.body.progress || 0;
+
+	Task.findOneAndUpdate({ _id: taskId }, {
+		$set: {
+			progress: parseInt(progress)
+		}
+	})
+		.then((e) => {
+			res.status(200).send("update progress successfully!");
+		})
+		.catch((e) => {
+			res.status(500).send("update progress failed!");
+		})
+});
+router.patch("/:task_id", (req, res) => {
+	let taskId = req.params.task_id;
+	let contributor = req.body.contributor || "";
+	Task.findOneAndUpdate({ _id: taskId }, { $push: { contributors: contributor}})
+	.then((e)=> {
+		res.status(200).send("successfully update the contributor!");
+	})
+	.catch((e)=> {
+		res.status(500).send("fail to add contributor!");
+	});
 
 });
 
