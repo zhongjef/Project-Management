354544c61143e506d121b7246a94b0d09f58535c
wujunxuan <835918679@qq.com>
2019-12-02 01:22:40 -0500

    add team router

--- backend/models/Team.js
+++ backend/models/Team.js
@@ -2,15 +2,20 @@ const mongoose = require("mongoose");
 const Joi = require("@hapi/joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
+const TeamMission = 
+	new mongoose.Schema({
+		userId: {type: String},
+		userName: {type: String},
+		taskList: []})
+
 const Team = mongoose.model(
 	"Team",
 	new mongoose.Schema({
 		name: { type: String, required: true, minlength: 1, maxlength: 80 },
 		managers: [String],
-		contributors: [String],
+		contributors: { type: [TeamMission]}
 	})
 );
-
 function validateTeam(team) {
 	const schema = Joi.object({
 		name: Joi.string().min(1).max(80).required(),
