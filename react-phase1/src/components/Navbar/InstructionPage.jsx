a2433e119cc6e4cc9f6b3a6e05c98c4ff0d0a89c
kch3coo <xia0weimo1999@gmail.com>
2019-12-03 20:33:33 -0500

    adding instruction section

--- react-phase1/src/components/Navbar/InstructionPage.jsx
+++ react-phase1/src/components/Navbar/InstructionPage.jsx
@@ -43,8 +43,14 @@ export default class InstructionPage extends Component {
             <Modal.Title>Instruction to our Project</Modal.Title>
           </Modal.Header>
           <Modal.Body>
+              
+  Hi! Welcome to our Project Management Website!
+We include features that can help you optimize your group work! 
+You can simply start by creating a project, or go through all the example projects.
+If you have been invited to a project, it will appear on the Contribute to Project Section.
+Remember to complete team tasks as a member of the Contributor! If you want to start your own project, start by clicking the main button on the right-hand side. You can fill in all the data and descriptions of your project. Remember to add teams to your project. To manage your project, you can simply click on any one of them on the Manage My Project Section. You can also create your team or assign tasks to members over there. We currently do not support the feature of searching and adding a member to your team, but more features are on the way!
 
-              <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> 
+              <br/> <br/> <br/>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={() => this.close()}>Close</Button>
