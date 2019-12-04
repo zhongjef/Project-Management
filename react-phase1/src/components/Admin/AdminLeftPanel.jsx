80c92ae893e75d856c452fd20e879854a0a3347b
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2019-12-03 19:24:38 -0500

    Remove extra code for team

--- react-phase1/src/components/Admin/AdminLeftPanel.jsx
+++ react-phase1/src/components/Admin/AdminLeftPanel.jsx
@@ -3,63 +3,38 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Redirect } from "react-router-dom";
 
 class LeftPanel extends Component {
-  state = {
-    showUsers: true
-  };
-  render() {
-    return (
-      <List-group className="d-inline-block">
-        <button
-          onClick={this.handleUsersTab}
-          className={this.getUserTabClassName()}
-        >
-          Users
-        </button>
-        <button
-          onClick={this.handleTeamsTab}
-          className={this.getTeamTabClassName()}
-        >
-          Teams
-        </button>
-        <button className="btn btn-danger list-group-item list-group-item-action list-group-item-danger text-center ">
-          <a href="/" className="d-block text-danger">
-            Logout
-          </a>
-        </button>
-      </List-group>
-    );
-  }
+	state = {
+		showUsers: true
+	};
+	render() {
+		return (
+			<List-group className="d-inline-block">
+				<button onClick={this.handleUsersTab} className={this.getUserTabClassName()}>
+					Users
+				</button>
+				<button className="btn btn-danger list-group-item list-group-item-action list-group-item-danger text-center ">
+					<a href="/" className="d-block text-danger">
+						Logout
+					</a>
+				</button>
+			</List-group>
+		);
+	}
 
-  getUserTabClassName() {
-    let className =
-      "rounded-0 text-center list-group-item list-group-item-action ";
-    if (this.state.showUsers) {
-      className += "active";
-    }
-    return className;
-  }
+	getUserTabClassName() {
+		let className = "rounded-0 text-center list-group-item list-group-item-action ";
+		if (this.state.showUsers) {
+			className += "active";
+		}
+		return className;
+	}
 
-  getTeamTabClassName() {
-    let className = "text-center list-group-item list-group-item-action ";
-    if (!this.state.showUsers) {
-      className += "active";
-    }
-    return className;
-  }
-
-  handleUsersTab = () => {
-    this.setState({
-      showUsers: true
-    });
-    this.props.handleClick("users");
-  };
-
-  handleTeamsTab = () => {
-    this.setState({
-      showUsers: false
-    });
-    this.props.handleClick("teams");
-  };
+	handleUsersTab = () => {
+		this.setState({
+			showUsers: true
+		});
+		this.props.handleClick("users");
+	};
 }
 
 export default LeftPanel;
